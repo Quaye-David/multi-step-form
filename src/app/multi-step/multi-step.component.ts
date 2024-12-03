ff5c2eaeae2b91fad4a3ea3b980511b6df5d3133
DNA_Q <david.quaye@amalitech.com>
2024-12-03 15:24:02 +0000

    refactor multi-step form components and remove deprecated files

---
+++ src/app/multi-step/multi-step.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-multi-step',
+  standalone: true,
+  imports: [],
+  templateUrl: './multi-step.component.html',
+  styleUrl: './multi-step.component.css'
+})
+export class MultiStepComponent {
+
+}
