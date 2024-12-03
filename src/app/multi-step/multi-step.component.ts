5b26d91f670b6e08ebe5a90e6f729a83b1498d16
DNA_Q <david.quaye@amalitech.com>
2024-12-03 16:25:47 +0000

    enhance multi-step component with active link styling and add global font styles

--- src/app/multi-step/multi-step.component.ts
+++ src/app/multi-step/multi-step.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-multi-step',
   standalone: true,
-  imports: [RouterLink],
-  templateUrl: './multi-step.component.html',
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+templateUrl: './multi-step.component.html',
   styleUrl: './multi-step.component.css'
 })
 export class MultiStepComponent {
